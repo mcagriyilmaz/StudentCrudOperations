c51e64ecb53ac56d29395091df7de25ae69239c3
mcagriyilmaz <70220771+mcagriyilmaz@users.noreply.github.com>
2023-08-15 11:16:09 +0300

    Add files via upload

---
+++ database/script.sql
@@ -0,0 +1,14 @@
+create table STUDENTS
+(
+    FIRSTNAME   VARCHAR(40),
+    LASTNAME    VARCHAR(40),
+    PHONE       VARCHAR(20),
+    ID          INTEGER identity
+        constraint "STUDENTS_pk"
+            primary key,
+    CITY        VARCHAR(20),
+    DISTRICT    VARCHAR(50),
+    DESCRIPTION VARCHAR(200)
+);
+
+
